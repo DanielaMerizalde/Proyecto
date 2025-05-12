00dd5d05bbc45b2457826f61723d335c27f98f80
DanielaMerizalde <dmerizalde9898@gmail.com>
2025-05-12 16:32:08 -0500

    CMSSW information

---
+++ CMSSW/Sim/python/phidm.py
@@ -0,0 +1,26 @@
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8HadronizerFilter",
+    comEnergy = cms.double(13000.0),
+    crossSection = cms.untracked.double(1.0),
+    filterEfficiency = cms.untracked.double(1.0),
+    maxEventsToPrint = cms.untracked.int32(-1),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        processParameters = cms.vstring(
+	    '51:all = on',
+	    '51:oneChannel = 1 1.0 2 -2',
+        ),
+        parameterSets = cms.vstring(
+            'pythia8CommonSettings',
+            'pythia8CUEP8M1Settings',
+            'processParameters',
+        )
+    )
+)
+
+ProductionFilterSequence = cms.Sequence(generator)
